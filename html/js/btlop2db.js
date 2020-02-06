21a084d81c5d00a3b44cd11b2aeb6269de604eeb
ratty27 <shinobu27@gmail.com>
2020-02-06 15:01:37 +0900

    ハッシュの更新です。

--- html/js/btlop2db.js
+++ html/js/btlop2db.js
@@ -1589,11 +1589,11 @@ function init()
 	var	recv_skill = function(x)     { db_skill = csv_to_db(x).sort('name');          updateMSList(true); }
 	var	recv_custom_parts = function(x) { db_custom_parts = csv_to_db(x).sort('name'); updateMSList(true); }
 	var	recv_enhancement = function(x)  { db_enhancement = csv_to_db(x).sort('name');  updateMSList(true); }
-	read_file( "db/btlop2_MS.csv?v=c8e83407414e1af45f1aa770ba2b2d71cfcb41aa", recv_ms );
+	read_file( "db/btlop2_MS.csv?v=6b3368c07a2ccca82be669ce5e463ac4cd50d21b", recv_ms );
 	read_file( "db/btlop2_Weapon1.csv?v=e547c311becdd1b060f97ae98f51f167f6d35ebd", recv_weapon1 );
 	read_file( "db/btlop2_Weapon2.csv?v=cde36b89acc7ce374dbcba3161e34599701a9348", recv_weapon2 );
-	read_file( "db/btlop2_SubWeapon.csv?v=8c00c41e3860d5cabfbbf176d285fa0b0dfeb5aa", recv_subweapon );
-	read_file( "db/btlop2_Skill.csv?v=23c3a2d38367942983296f3662779c523d207676", recv_skill );
+	read_file( "db/btlop2_SubWeapon.csv?v=5885b1dad41dd0a4db69c15581ba784f2b8e98fe", recv_subweapon );
+	read_file( "db/btlop2_Skill.csv?v=01cb3531e98720053e68120bd5dc40e31571c34c", recv_skill );
 	read_file( "db/btlop2_CustomParts.csv?v=9d488c5af0f83e56fb589c5c02fd275ec1432424", recv_custom_parts );
 	read_file( "db/btlop2_Enhancement.csv?v=4c5a038802c79f44d2884343d9889350ef7eb3bc", recv_enhancement );
 
